1642966836db94caf4d2e47e61da426fb77d0946
adlai88 <adlai88@gmail.com>
2024-10-22 14:51:14 +0800

    fix deployment issues

--- emoji-maker/components/ClientSideComponent.tsx
+++ emoji-maker/components/ClientSideComponent.tsx
@@ -4,7 +4,6 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function ClientSideComponent() {
-  const router = useRouter();
 
   useEffect(() => {
     const createUserProfile = async () => {
